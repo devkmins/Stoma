96ec2268f0d49e769e247bffec93818043004f1e
Min <esspacetoM@gmail.com>
2023-12-19 23:34:39 +0900

    init: create-next-app 설정

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Fico",
+  description: "Fico는 증권 정보를 제공하여 회사를 발굴할 때 도움을 줍니다.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
